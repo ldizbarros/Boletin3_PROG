2f3df174cb2db4bfc2ea2a107bad54107af9e1e9
ldizbarros <ldizbarros@victoria13.danielcastelao.org>
2017-10-26 09:37:03 +0200

    Inicio Proyecto

---
+++ Boletin3_2/src/boletin3_2/Boletin3_2.java
@@ -0,0 +1,13 @@
+package boletin3_2;
+
+public class Boletin3_2 {
+
+    public static void main(String[] args) {
+        Satelite s1 = new Satelite();
+        s1.verPosicion();
+        
+        Satelite s2 = new Satelite(20,48,75);
+        s2.verPosicion();
+    }
+    
+}
